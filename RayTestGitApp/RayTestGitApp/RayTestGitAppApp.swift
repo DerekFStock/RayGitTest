e9f6a31a1f16aa6cca821f25d1e2e5a7da9fc3ca
Derek Stock <derekstock@gmail.com>
2026-06-07 15:27:36 -0700

    First project commit

---
+++ RayTestGitApp/RayTestGitApp/RayTestGitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RayTestGitAppApp.swift
+//  RayTestGitApp
+//
+//  Created by Derek Stock on 6/7/26.
+//
+
+import SwiftUI
+
+@main
+struct RayTestGitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
